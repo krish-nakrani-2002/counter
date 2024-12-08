413d17a219053b8ca7e347e991ca520457d7ce6b
krish-nakrani-2002 <nakranikrish5@gmail.com>
2024-12-08 18:54:53 +0530

    add buttons

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,30 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let Counter = 5
+
+  const addValue = () => {
+    console.log("clicked", Counter);
+    Counter = Counter + 1 
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>Krish Nakrani</h1>
+     <h2>Counter Value: {Counter}</h2>
+
+     <button onClick={addValue}>Add Value</button>
+     <button>Remove Value</button>
     </>
   )
 }
