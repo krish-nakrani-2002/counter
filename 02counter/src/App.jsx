ce5f9b8713f21c2d0c9917e0fdee4a975614195a
krish-nakrani-2002 <nakranikrish5@gmail.com>
2024-12-08 19:33:18 +0530

    useState hook

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -5,11 +5,16 @@ import './App.css'
 
 function App() {
 
-  let Counter = 5
+  let [Counter, setCounter] = useState(0)
 
-  const addValue = () => {
-    console.log("clicked", Counter);
-    Counter = Counter + 1 
+  //let Counter = 5
+
+  const addValue = () => { 
+    setCounter(Counter + 1)
+  }
+
+  const removeValue = () => {
+    setCounter(Counter - 1)
   }
 
   return (
@@ -17,8 +22,8 @@ function App() {
      <h1>Krish Nakrani</h1>
      <h2>Counter Value: {Counter}</h2>
 
-     <button onClick={addValue}>Add Value</button>
-     <button>Remove Value</button>
+     <button onClick={addValue}>Add Value {Counter}</button>
+     <button onClick={removeValue}>Remove Value {Counter}</button>
     </>
   )
 }
